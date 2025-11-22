88e03447ee82326e77b2c56fa08fe2c497eb5a83
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-11-22 13:32:56 +0530

    Create After Fetch

--- src/Js/Create/Vertical/FormLoad/DOMContentLoaded/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/status200.js
+++ src/Js/Create/Vertical/FormLoad/DOMContentLoaded/AddListeners/SaveButtonId/FetchFromFuncs/AfterFetch/status200.js
@@ -4,16 +4,4 @@ const StartFunc = ({ inRowPk }) => {
     window.location.href = `${UrlJson.RedirectToUrl}?inRowPk=${inRowPk}`;
 };
 
-const LocalFuncForSingleTable = ({ inRowPk }) => {
-    if (window.location.pathname.endsWith(`/${UrlJson.PresentUrl}`)) {
-        window.location.href = `${UrlJson.RedirectToUrl}?inRowPk=${inRowPk}`;
-        return true;
-    }
-    return false;
-};
-
-const LocalFuncForAllTables = ({ inRowPk }) => {
-    window.location.href = `${UrlJson.RedirectToUrl}?inRowPk=${inRowPk}`;
-};
-
 export { StartFunc };
