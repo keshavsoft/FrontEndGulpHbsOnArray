7f8733981e1e574e5da8eada7f8bb58dd58713d5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2026-01-12 15:13:18 +0530

    UserName image added and some more

--- src/Js/Authentication/SignIn/FormLoad/DOMContentLoaded/funcToRun.js
+++ src/Js/Authentication/SignIn/FormLoad/DOMContentLoaded/funcToRun.js
@@ -2,6 +2,7 @@ import { StartFunc as StartFuncAddListeners } from "./AddListeners/startFunc.js"
 
 const StartFunc = async () => {
     StartFuncAddListeners();
+    localStorage.clear()
 };
 
 export { StartFunc };
